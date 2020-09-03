a68084e909872ee983d540cf866d908248fac83a
Shahin Eskandari <esi.018111@yahoo.com>
2020-09-03 12:26:33 +0430

    Update proxyscript.php

--- proxyscript.php
+++ proxyscript.php
@@ -4,17 +4,21 @@
     echo get_page($_GET['url']);
     //echo get_page("http://www.google.com");
     function get_page($url){
+        
+        $proxy = 'http://tor.dastpour.ge:9050';
+        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$url);
-        
-        $proxy = 'http://tor.dastpour.ge:3128';
+        curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5 );
         curl_setopt($ch, CURLOPT_PROXY, $proxy);
-        
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $data = curl_exec($ch);
         curl_close($ch);
+        
         return $data;
     }
+
+
 ?>
